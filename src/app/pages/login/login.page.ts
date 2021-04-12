c75dcdf80674e3f553c823fec71dc5c791ebfa20
Ayn <201811226@gordoncollege.edu.ph>
2021-04-12 19:16:28 +0800

    login error handler

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,28 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  name:string = 'Ayn'
   uname:string = "admin"
   pword:string = "admin"
+  unameInput:string;
+  pwordInput:string;
+  constructor(private user:UserService, private router:Router,public alertController: AlertController) { }
 
-  constructor(private user:UserService, private router:Router) { }
+  async presentAlert(title, sub) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: title,
+      subHeader: sub,
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.router.navigate(['/home']);
+  }
+
 
   ngOnInit() {
   }
@@ -19,13 +38,26 @@ export class LoginPage implements OnInit {
   login(e): void{
     e.preventDefault();
 
-    if(this.uname == e.target.uname.value && this.pword == e.target.pword.value){
-      console.log(e.target.uname.value);
-    console.log(e.target.pword.value);
-      this.user.setLogin();
-      this.router.navigate(['/home']);
-    }else{
-      this.router.navigate(['']);
+
+    if(this.unameInput != '' && this.pwordInput != ''){
+      if(this.uname == this.unameInput && this.pword == this.pwordInput ){
+        console.log(this.unameInput);
+        console.log(this.pwordInput);
+        this.presentAlert('Success','Welcome '+ this.name)
+        this.user.setLogin();
+
+      }else{
+        if(this.unameInput == null || this.pwordInput == null){
+          this.presentAlert('Warning','Fill the fields!')
+        }
+        if(this.unameInput != null && this.pwordInput != null){
+          this.presentAlert('Warning','Username or Password is incorrect')
+        }
+
+
+      }
     }
+
+
   }
 }
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if(this.user.isLoggedIn()){
-        this.router.navigate(['home']);
+        this.router.navigate(['/home']);
         return false;
       }
 
